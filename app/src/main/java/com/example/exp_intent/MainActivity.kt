ea8b28ade476404663ef15ef0646b66c3dee882c
Zubair Rashid <zubair.rashid@thetowertech.com>
2024-02-20 10:20:00 +0500

    Adding Files of the Project

--- app/src/main/java/com/example/exp_intent/MainActivity.kt
+++ app/src/main/java/com/example/exp_intent/MainActivity.kt
@@ -9,7 +9,7 @@ import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
+        super.onCreate(savedInstanceState) 
         setContentView(R.layout.activity_main)
 
         val buttonLight=findViewById<Button>(R.id.BtnLight)
